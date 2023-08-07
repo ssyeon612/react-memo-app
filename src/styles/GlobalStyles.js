70e6dbc387fb9d70d4993e9d6c99982b5ebe814c
ssyeon612 <ssyeon612@gmai.com>
2023-08-07 15:52:50 +0900

    style: reset base css

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,37 @@
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+${reset}
+  * {
+    box-sizing: border-box;
+  }
+
+  html, body {
+    height: 100%;
+    font-family: 'Noto Sans KR', sans-serif;
+    font-weight: 300;
+    font-size: 16px;
+  }
+
+  #root, .App {
+    height: inherit;
+    background-color: '#9E9FA5'
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  button, input, textarea, select, option {
+    font-family: inherit;
+    border: 0;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyles;
